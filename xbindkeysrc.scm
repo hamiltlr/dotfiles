562ae8c98a14bee5ff81c0687e3596a56f863293
Lee Hamilton <lhamil64@gmail.com>
2022-01-17 16:57:45 -0500

    Created xbindkeys script for mouse zoom buttons

--- xbindkeysrc.scm
+++ xbindkeysrc.scm
@@ -1,12 +1,24 @@
 ; From https://wiki.archlinux.org/title/Xbindkeys#Mouse_Chording
+;
+; Modifications:
+;  - The original behavior was to trigger the command when the modifier
+;    was released. I changed it to do the command as soon as the key is
+;    released (not the modifier), so you can hold the modifier and trigger
+;    the command multiple times.
+;  - Added support to run a command when just the modifier key is pressed
 
-(define (define-mouse-chords chord-key . definitions)
+(define (define-mouse-chords chord-key mod-single-cmd . definitions)
   (define (start-mouse-chord)
-    (let ((cmd #f))
+    (let ((userPressedKey 0))
       (for-each
         (lambda (definition)
           (let ((key (list-ref definition 0)) (binding (list-ref definition 1)))
-            (xbindkey-function key (lambda () (set! cmd binding)))))
+            (xbindkey-function key 
+                (lambda ()
+                  (set! userPressedKey 1)
+                  (run-command binding)
+                  )
+                  )))
         definitions)
       (xbindkey-function `(release ,chord-key)
         (lambda ()
@@ -16,14 +28,15 @@
               (let ((key (list-ref definition 0)))
                 (remove-xbindkey key)))
             definitions)
-          (if cmd
-            (begin
-              (run-command cmd)
-              (set! cmd #f))
-          )))))
+          (if (= userPressedKey 0)
+                (run-command mod-single-cmd)
+              (set! userPressedKey 0) ;else
+              )
+          ))))
   (xbindkey-function chord-key start-mouse-chord))
 
-(define-mouse-chords "b:10"
-  (list '(release "b:1") "xdotool click 8")
-  (list '(release "b:2") "xdotool click 9")
+(define-mouse-chords "b:13"
+  "xdotool key alt+super+equal"
+  (list '(release "b:8") "xdotool key alt+super+minus")
+  (list '(release "b:9") "xdotool key alt+super+Escape")
 )
